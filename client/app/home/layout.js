0a9aa9beeeb56cce440ff1ee5c0eae8221704ba5
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-22 18:32:08 +0530

    sidebar space and sm: styling added

--- client/app/home/layout.js
+++ client/app/home/layout.js
@@ -5,17 +5,17 @@ import Sidebar from "../ui/Sidebar"
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>
-                <header>
+            <body className="sm:flex sm:h-screen sm:overflow-hidden gap-10">
+                <header className="sm:overflow-y-auto ">
                     <nav className="md:hidden">
                         <NavBar/>
                     </nav>
-                    <nav>
+                    <nav className="min-w-[300px] bg-slate-300">
                         <Sidebar/>
                     </nav>
                 </header>
 
-                <main> 
+                <main className="sm:overflow-y-auto"> 
                     {children}
                 </main>
             </body>
--- client/app/ui/Sidebar.js
+++ client/app/ui/Sidebar.js
@@ -1,13 +1,17 @@
 'use client'
+import { keania } from '../fonts'
 
 import React from 'react'
 
 export default function Sidebar() {
     return (
         <div>
-            <div>
-                
-            </div>
+            <div className='text-4xl text-white bg-black h-[170px] flex items-end p-5'>
+                <div className={`${keania.className}`}>
+                    <span className='text-[55px]'>N</span>
+                    exus
+                </div>
+           </div>
         </div>
     )
 }
