9e1965e8d33afc50b33f0d237935faff24b760d2
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-22 18:42:53 +0530

    layout error fixed

--- client/app/home/layout.js
+++ client/app/home/layout.js
@@ -5,19 +5,21 @@ import Sidebar from "../ui/Sidebar"
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className="sm:flex sm:h-screen sm:overflow-hidden gap-10">
-                <header className="sm:overflow-y-auto ">
-                    <nav className="md:hidden">
-                        <NavBar/>
-                    </nav>
-                    <nav className="min-w-[300px] bg-slate-300">
-                        <Sidebar/>
-                    </nav>
-                </header>
+            <body >
+                <div className="flex flex-col sm:flex-row sm:h-screen sm:overflow-hidden gap-10">
+                    <header className="sm:overflow-y-auto ">
+                        <nav className="md:hidden">
+                            <NavBar/>
+                        </nav>
+                        <nav className="min-w-[300px] max-w-[500px] bg-slate-300">
+                            <Sidebar/>
+                        </nav>
+                    </header>
 
-                <main className="sm:overflow-y-auto"> 
-                    {children}
-                </main>
+                    <main className="sm:overflow-y-auto"> 
+                        {children}
+                    </main>
+                </div>
             </body>
         </html>
     )
