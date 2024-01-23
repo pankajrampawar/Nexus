f3cc171f2b7710680fe4b816616a9a0db0b02185
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-23 19:41:53 +0530

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
-
 const express = require('express')
 const app = express()
+const connectDB = require('./db')
 const port = 8080;
 
 app.get('/', (req, res) => {
     res.send("backend running on port 8080");
 });
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
