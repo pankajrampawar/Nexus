b2d08e12439feabf082086d643e80443b300cdb7
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-23 18:57:39 +0530

    fonts added to the top bar

--- client/app/home/page.js
+++ client/app/home/page.js
@@ -5,7 +5,7 @@ import TopBar from "../ui/TopBar"
 export default function Home() {
     return (
         <main className="overflow-hidden h-full relative">
-            <div className="absolute text-white flex  gap-8 backdrop-blur-sm softBlack w-full rounded-lg">
+            <div className="absolute text-white flex  gap-8 backdrop-blur-sm softBlack w-full rounded-xl">
                 <TopBar/>
             </div>
             <div className="overflow-y-auto h-full">
--- client/app/ui/TopBar.js
+++ client/app/ui/TopBar.js
@@ -1,23 +1,25 @@
 'use client'
 
 import React from 'react'
+import { Kenia } from 'next/font/google'
+import { keania } from '../fonts'
 
 export default function TopBar() {
     return (
-        <div className="w-full h-full rounded-lg flex justify-between">
-            <div className='flex-1  flex justify-center items-center p-3'>
-                <div>
-                    Something
+        <div className={`w-full h-full rounded-xl flex justify-between font-semibold tracking-wider text-lg ${keania.className}`}>
+            <div className='flex-1  flex justify-center items-center p-2 cursor-pointer'>
+                <div className='cursor-pointer'>
+                    General
                 </div>
             </div>
-            <div className='flex-1  flex justify-center items-center p-3'>
+            <div className='flex-1  flex justify-center items-center p-2 softDarkBlack rounded-2xl cursor-pointer'>
                 <div>
-                    Something
+                    Hidden
                 </div>
             </div>
-            <div className='flex-1  flex justify-center items-center p-3'>
+            <div className='flex-1  flex justify-center items-center p-2 cursor-pointer'>
                 <div>
-                    Something
+                    Polls
                 </div>
             </div>
         </div>
